b07d85c18d9e0ea119ffabd1539b6bccc9b189aa
anonymouse <anonymouse@x.com>
2021-09-12 16:06:31 +0800

    update

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 import json
 import glob
 
+from collections import defaultdict
+
 json_files = sorted(glob.glob('./*.json'))
 
 capitals = {}
@@ -33,17 +35,41 @@ for json_file in json_files:
 
                 capitals[tag][dt_string].append(symbol)
 
+dt_strings = []
+
 for capital, history in capitals.items():
     print(f'dump capital data {capital}({len(history)})')
 
     for dt_string, value in history.items():
+        dt_strings.append(dt_string)
+
         history[dt_string] = {
             'count': len(value),
-            'data': sorted(value)
+            'symbols': sorted(value)
         }
         
     with open(f'capitals/{capital}.json', 'w', encoding='utf-8') as f:
         json.dump(history, f, indent=2, ensure_ascii=False)
 
+holding_history = {}
+dt_strings = sorted(set(dt_strings))
 
+for dt_string in dt_strings:
+    if not dt_string in holding_history:
+        holding_history[dt_string] = defaultdict(lambda: 0)
 
+    for capital, history in capitals.items():
+        if not dt_string in history:
+            continue
+
+        for symbol in history[dt_string]['symbols']:
+            holding_history[dt_string][symbol] += 1
+    
+    holdings = holding_history[dt_string]
+    sorted_holdings = dict(sorted(holdings.items(), key=lambda x: x[1], reverse=True))
+    holding_history[dt_string] = sorted_holdings
+
+print(f'dump holding history data ({len(holding_history)})')
+
+with open(f'capitals/holding-history.json', 'w', encoding='utf-8') as f:
+    json.dump(holding_history, f, indent=2, ensure_ascii=False)
